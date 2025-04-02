609f219337da3f443a4bf5c457588901b875dc64
kkconaty23 <conakp@farmingdale.edu>
2025-04-02 11:49:27 -0400

    commit

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,6 @@ module org.example.circuit_project {
     requires javafx.controls;
     requires javafx.fxml;
     requires firebase.admin;
-    requires com.google.auth.oauth2;
     requires com.google.auth;
     requires google.cloud.firestore;
 
