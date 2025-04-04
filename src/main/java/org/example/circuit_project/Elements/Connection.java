c4fa75fa7a69ce976fc402a0ead4401e3b5ab1c3
SandalCodez <167233548+SandalCodez@users.noreply.github.com>
2025-04-04 01:49:05 -0400

    - Created and completed circuit class - Created component, connection and port class with commented guidelines for the next developer

---
+++ src/main/java/org/example/circuit_project/Elements/Connection.java
@@ -0,0 +1,13 @@
+package org.example.circuit_project.Elements;
+//Each component needs to connect to another(or else whats the point). Ex. a battery(2 connections ports) will connect to a
+//lightbulb(also 2) via a wire(2 ports) SOOOOOOOO each component here will have ports with connections
+//  in between other ports. so YEAH, this class will take care of that
+//Each connection should have the following:
+//- a unique id
+//- sourceComponenetID( the ID of the component that this connection is coming from)
+//- sourcePortID (the ID of the port where theconnection is coming from)
+//- targetComponentID( same thing as source but, yanno, for the target)
+//- targetPortID( that stuff ^^ up there but target)
+
+public class Connection {
+}
