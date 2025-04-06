f4810436d20adcc55dad13a2e61c16bbf3ce31c1
SandalCodez <167233548+SandalCodez@users.noreply.github.com>
2025-04-06 02:53:12 -0400

    - bare minimum POJO made for Connection.java, Component.java, Port.java - NEEDS COMMENTS **These classes may not be used in storage integration, but instead a potential component stat feature -esteban **

--- src/main/java/org/example/circuit_project/Elements/Component.java
+++ src/main/java/org/example/circuit_project/Elements/Component.java
@@ -1,4 +1,8 @@
 package org.example.circuit_project.Elements;
+
+
+import java.util.List;
+
 // each component will be a  type (battery, wire, lightbulb, fuse, switch, resistor)
 //component will have the following:
 //- a unique ID
@@ -7,4 +11,67 @@ package org.example.circuit_project.Elements;
 //- properties (voltage, resistance)
 //- a list of ports
 public class Component {
+    private int id;
+    private String name = "default";
+    private int xCoord;
+    private int yCoord;
+    private float rotate;
+    private String properties;
+    private List<Port> portList;
+
+    public Component(int id, String name, int xCoord, int yCoord, float rotate, String properties, List<Port> portList) {
+
+        //NOTE:Unique idea on separate machine
+        this.id = id;
+
+
+        this.name = name;
+        this.xCoord = xCoord;
+        this.yCoord = yCoord;
+        this.rotate = rotate;
+        this.properties = properties;
+        this.portList = portList;
+    }
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public int getxCoord() {
+        return xCoord;
+    }
+    public void setxCoord(int xCoord) {
+        this.xCoord = xCoord;
+    }
+    public int getyCoord() {
+        return yCoord;
+    }
+    public void setyCoord(int yCoord) {
+        this.yCoord = yCoord;
+    }
+    public float getRotate() {
+        return rotate;
+    }
+    public void setRotate(float rotate) {
+        this.rotate = rotate;
+    }
+    public String getProperties() {
+        return properties;
+    }
+    public void setProperties(String properties) {
+        this.properties = properties;
+    }
+    public List<Port> getPortList() {
+        return portList;
+    }
+    public void setPortList(List<Port> portList) {
+        this.portList = portList;
+    }
 }
--- src/main/java/org/example/circuit_project/Elements/Connection.java
+++ src/main/java/org/example/circuit_project/Elements/Connection.java
@@ -4,10 +4,53 @@ package org.example.circuit_project.Elements;
 //  in between other ports. so YEAH, this class will take care of that
 //Each connection should have the following:
 //- a unique id
-//- sourceComponenetID( the ID of the component that this connection is coming from)
+//- sourceComponentID( the ID of the component that this connection is coming from)
 //- sourcePortID (the ID of the port where theconnection is coming from)
 //- targetComponentID( same thing as source but, yanno, for the target)
 //- targetPortID( that stuff ^^ up there but target)
 
 public class Connection {
+    private int connectionID;
+    private int sourceComponentID;
+    private int sourcePortID;
+    private int targetComponentID;
+    private int targetPortID;
+
+    public Connection(int connectionID, int sourceComponentID, int sourcePortID, int targetComponentID, int targetPortID) {
+        this.connectionID = connectionID;
+        this.sourceComponentID = sourceComponentID;
+        this.sourcePortID = sourcePortID;
+        this.targetComponentID = targetComponentID;
+        this.targetPortID = targetPortID;
+    }
+    public int getConnectionID() {
+        return connectionID;
+    }
+    public void setConnectionID(int connectionID) {
+        this.connectionID = connectionID;
+    }
+    public int getSourceComponentID() {
+        return sourceComponentID;
+    }
+    public void setSourceComponentID(int sourceComponentID) {
+        this.sourceComponentID = sourceComponentID;
+    }
+    public int getSourcePortID() {
+        return sourcePortID;
+    }
+    public void setSourcePortID(int sourcePortID) {
+        this.sourcePortID = sourcePortID;
+    }
+    public int getTargetComponentID() {
+        return targetComponentID;
+    }
+    public void setTargetComponentID(int targetComponentID) {
+        this.targetComponentID = targetComponentID;
+    }
+    public int getTargetPortID() {
+        return targetPortID;
+    }
+    public void setTargetPortID(int targetPortID) {
+        this.targetPortID = targetPortID;
+    }
 }
